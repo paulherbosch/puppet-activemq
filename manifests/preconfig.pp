f2a1a79270ff135e7e05c73e24f8d98066f8230b
Dieter De Meyer <dieterdemeyer@gmail.com>
2015-01-06 15:00:51 +0100

    Add preconfig to run before package install

--- manifests/preconfig.pp
+++ manifests/preconfig.pp
@@ -1,46 +1,16 @@
-class activemq::config(
-  $version = undef,
-  $persistence_db_driver_version = '6'
-){
+class activemq::preconfig {
 
-  $major_version_withoutrelease = regsubst($version, '^(\d+\.\d+)\.\d+-.*$','\1')
-  notice("major_version_withoutrelease=${major_version_withoutrelease}")
-
-  file { '/data/activemq':
-    ensure => directory,
-    owner  => 'activemq',
-    group  => 'activemq'
+  file { '/etc/activemq':
+    ensure => directory
   }
 
   file { '/etc/activemq/activemq.xml':
     ensure  => file,
     mode    => '0644',
     content => template("${module_name}/activemq.xml.erb"),
-    notify  => Class['activemq::service']
-  }
-
-  file { '/etc/activemq/activemq-wrapper.conf':
-    ensure  => file,
-    mode    => '0644',
-    content => template("${module_name}/v${major_version_withoutrelease}/activemq-wrapper.conf.erb"),
-    notify  => Class['activemq::service']
-  }
-
-  file { '/etc/activemq/jetty-realm.properties':
-    ensure  => file,
-    mode    => '0644',
-    content => template("${module_name}/jetty-realm.properties.erb"),
-    notify  => Class['activemq::service']
-  }
-
-  file { "/usr/share/activemq/lib/ojdbc${persistence_db_driver_version}.jar":
-    ensure => file,
-    source => "puppet:///modules/${module_name}/drivers/oracle/ojdbc${persistence_db_driver_version}.jar"
-  }
-
-  file { '/usr/share/activemq/lib/mysql-connector-java.jar':
-    ensure => file,
-    source => "puppet:///modules/${module_name}/drivers/mysql/mysql-connector-java-5.1.33.jar"
+    replace => false,
+    notify  => Class['activemq::service'],
+    require => File['/etc/activemq']
   }
 
 }
